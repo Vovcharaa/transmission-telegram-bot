cf71e7af71401b56033e16973fa87f9a061b3926
Nazar Vovk <nazarii.vovk@chatbots.studio>
2020-10-28 19:51:32 +0200

    Init project setup

---
+++ transsmision-telegram-bot/__main__.py
@@ -0,0 +1,4 @@
+from .app import TranssmissionTelegramBot
+
+if __name__ == '__main__':
+    TranssmissionTelegramBot.run()
---
+++ transsmision-telegram-bot/app.py
@@ -0,0 +1,5 @@
+class TranssmissionTelegramBot:
+
+    @staticmethod
+    def run():
+        print("Hello World...")
